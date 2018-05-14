1a5d6c05b9a2f93a777435779c1319e9a5171728
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2018-05-14 10:50:31 +0200

    first commit

---
+++ ViperContact/AppDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  AppDelegate.swift
+//  ViperContact
+//
+//  Created by Alan Casas Arevalo on 14/5/18.
+//  Copyright © 2018 Alan Casas Arevalo. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        window?.rootViewController = UIViewController()
+        
+        window?.backgroundColor = UIColor.blue
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {}
+
+    func applicationDidEnterBackground(_ application: UIApplication) {}
+
+    func applicationWillEnterForeground(_ application: UIApplication) {}
+
+    func applicationDidBecomeActive(_ application: UIApplication) {}
+
+    func applicationWillTerminate(_ application: UIApplication) {}
+
+
+}
+
